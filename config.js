967be0ba8e878c687f5e1bf6ff7d5e930d7946b3
SHANUWA-MD-V3 <sudeshm884@gmail.com>
2024-12-03 00:25:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session is",  //session id dhan
+SESSION_ID: process.env.SESSION_ID || "Hd4HRACa#1C8xIjxISmp3GiEJD2Uc04hYezpqS84jOy3mPDv-uTU",  //session id dhan
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/2qD0Syx/6648.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
 SUDO_NB: process.env.SUDO_NB || "94774609569",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 AUTO_VOICE:"true"
 };
 
